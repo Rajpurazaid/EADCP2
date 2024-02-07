d08ec48a429198088a7dbfa98093176440631156
Rajpurazaid11 <zaidrajpura88@gmail.com>
2024-02-07 09:04:59 +0530

    Zaid

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const getCurrentDateTime = require('./dateTimeModule');
+
 const app = express();
+const port = 3500;
+
 app.get('/', (req, res) => {
-    res.send('Hello, this is a code by Zaid Rajpura to test pipeline and this is the second version');
-  });
-  const port = process.env.PORT || 3000;
+  const currentDateTime = getCurrentDateTime();
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Current Date and Time</title>
+    </head>
+    <body>
+    <h1>Current Date and Time</h1>
+    <p>${currentDateTime}</p>
+  </body>
+  </html>
+`;
+    res.send(html);
+});
 app.listen(port, () => {
-  console.log(`API server listening on port ${port}`);
+    console.log(`Server started at http://localhost:${port}`);
 });
